8696ccb5a801e762ee3b75293f1dadf8a43b35cb
AndyDaMandy <maldonado.andy.m@gmail.com>
2022-12-04 16:16:52 -0500

    set up friendship associations

---
+++ app/controllers/friendships_controller.rb
@@ -0,0 +1,6 @@
+class FriendshipsController < ApplicationController
+    
+    def index
+    end
+    
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :posts
+  has_many :friendships
+  has_many :friends, through => :friendships
 end
