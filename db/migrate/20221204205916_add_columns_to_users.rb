fe587a683050616dc1d43ab67d062f4772e4bf02
AndyDaMandy <maldonado.andy.m@gmail.com>
2022-12-04 16:04:50 -0500

    created user model, added devise, created post scaffold

---
+++ db/migrate/20221204205916_add_columns_to_users.rb
@@ -0,0 +1,8 @@
+class AddColumnsToUsers < ActiveRecord::Migration[7.0]
+  def change
+    add_column :users, :username, :string, unique: true
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
+    add_column :users, :age, :integer
+  end
+end
