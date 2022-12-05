2c2f1d36b620579daa824bc977a154d7e7451d95
AndyDaMandy <maldonado.andy.m@gmail.com>
2022-12-04 21:05:50 -0500

    added inverse friends and bootstrap + navbar

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :posts
   has_many :friendships
-  has_many :friends, through => :friendships
+  has_many :friends, :through => :friendships
+  has_many :inverse_friendships, :class_name => "Friendship", :foreign_key => "friend_id"
+  has_many :inverse_friends, :through => :inverse_friendships, :source => :user
+
 end
