ad87ffe5de346ea78e6ac9dd6a20151f11ed7d55
AndyDaMandy <maldonado.andy.m@gmail.com>
2022-12-14 09:52:36 -0500

    began setting up profiles controller

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,7 +2,15 @@ class ProfilesController < ApplicationController
     def show
     end
     def new
+        @profile = Profile.new
     end
     def create
+        @profile = current_user.build_profile(profile_params)
     end
+
+    private
+    def profile_params
+        params.require(:profile).permit(:user_id, :first_name, :last_name, :age, :likes, :dislikes, :about)
+      end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_14_134717) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_14_135129) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -48,6 +48,19 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_14_134717) do
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
+  create_table "profiles", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.integer "age"
+    t.string "likes"
+    t.string "dislikes"
+    t.string "about"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_profiles_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -66,4 +79,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_14_134717) do
   add_foreign_key "likes", "posts"
   add_foreign_key "likes", "users"
   add_foreign_key "posts", "users"
+  add_foreign_key "profiles", "users"
 end
