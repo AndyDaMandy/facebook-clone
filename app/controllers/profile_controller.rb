35ca967266c165e39d686b59e39926424d9dfd55
AndyDaMandy <maldonado.andy.m@gmail.com>
2022-12-22 13:55:08 -0500

    setting up edit profiles

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -6,10 +6,13 @@ class ProfileController < ApplicationController
         @profile = @user.profile
     end
     def show
+      @profile = Profile.find(params[:id])
     end
     def new
       @user = current_user
+      if @user.profile == nil
       @profile = current_user.build_profile
+      end
     end
     def create
         @profile = current_user.build_profile(profile_params)
@@ -25,7 +28,21 @@ class ProfileController < ApplicationController
     end
     
     def edit
+      @user = current_user
+      @profile = current_user.profile
         
+    end
+
+    def update
+      respond_to do |format|
+        if @profile.update(post_params)
+          format.html { redirect_to post_url(@profile), notice: "Post was successfully updated." }
+          format.json { render :show, status: :ok, location: @profile }
+        else
+          format.html { render :edit, status: :unprocessable_entity }
+          format.json { render json: @profile.errors, status: :unprocessable_entity }
+        end
+      end
     end
 
     private
