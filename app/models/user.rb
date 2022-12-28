b7fedc976a3a9387569faa42a569ec84ad182ba6
AndyDaMandy <maldonado.andy.m@gmail.com>
2022-12-28 09:39:27 -0500

    set up profile views and fixing up friendship view

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,9 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :posts
   has_many :likes, dependent: :destroy
-  has_many :friendships
   has_one :profile
   accepts_nested_attributes_for :profile
+  has_many :friendships
   has_many :friends, :through => :friendships
   has_many :inverse_friendships, :class_name => "Friendship", :foreign_key => "friend_id"
   has_many :inverse_friends, :through => :inverse_friendships, :source => :user
