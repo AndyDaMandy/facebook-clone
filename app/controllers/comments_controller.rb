e0341fd380790d16eac522d9f7b9db0943747955
AndyDaMandy <maldonado.andy.m@gmail.com>
2023-01-11 10:26:59 -0500

    set up commenting system

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,11 +3,16 @@ class CommentsController < ApplicationController
     def create
         @post= Post.find(params[:post_id])
         @comment = @post.comments.create(comment_params)
-        redirect_to post_path(@post)
+        @comment.user_id = current_user.id
+        if @comment.save
+            redirect_to post_path(@post)
+        else
+            flash.now[:danger] = "error"
+        end
       end
     
       private
         def comment_params
-          params.require(:comment).permit(:commenter, :body)
+          params.require(:comment).permit(:comment)
         end
 end
