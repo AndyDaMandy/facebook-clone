5c06334a39492ff4dbe48b0b81852b0bca5fd79a
AndyDaMandy <maldonado.andy.m@gmail.com>
2023-01-11 15:25:18 -0500

    set up user view

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
     resources :likes
     resources :friendship
   end
+  resources :users, :only => [:show]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
