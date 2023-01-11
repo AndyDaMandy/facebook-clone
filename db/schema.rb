f687180b7dcf0b039bf18084bcb21afd65542c26
AndyDaMandy <maldonado.andy.m@gmail.com>
2023-01-11 14:31:09 -0500

    added in profile registration and editing

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_11_183934) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_11_192446) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -73,6 +73,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_11_183934) do
     t.string "likes"
     t.string "dislikes"
     t.string "about"
+    t.string "first_name"
+    t.string "last_name"
+    t.integer "age"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
