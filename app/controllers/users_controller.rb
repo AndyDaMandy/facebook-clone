ed18cb8fe47b2ad1aff81d5034ada5c4c6f18f8f
AndyDaMandy <maldonado.andy.m@gmail.com>
2023-01-12 08:19:05 -0500

    set up user profile!

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,28 @@ class UsersController < ApplicationController
     def show
         @user = User.find(params[:id])
     end
+
+    def edit
+        @user = current_user
+        #@profile = current_user.profile
+          
+      end
+  
+      def update
+        @user = current_user
+        respond_to do |format|
+          if @user.update(profile_params)
+            format.html { redirect_to user_url(@user), notice: "PRofile was successfully updated." }
+            format.json { render :show, status: :ok, location: @user }
+          else
+            format.html { render :edit, status: :unprocessable_entity }
+            format.json { render json: @user.errors, status: :unprocessable_entity }
+          end
+        end
+      end
+
+    private
+      def profile_params
+        params.require(:user).permit(:user_id, :first_name, :last_name, :age, :about)
+    end
 end
