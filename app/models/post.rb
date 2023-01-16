389a4f27a6c934463a31df2267c753a6e53df93b
AndyDaMandy <maldonado.andy.m@gmail.com>
2023-01-16 09:12:57 -0500

    added scope for current user

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,6 @@ class Post < ApplicationRecord
   belongs_to :user, dependent: :destroy
   has_many :likes, dependent: :destroy
   has_many :comments, dependent: :destroy
+
+  scope :filter_by_user_id, -> (user_id) { where(user_id: user_id)}
 end
