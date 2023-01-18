f07620b5c307608ddc52e64dcbeb2b7b6878bcf8
AndyDaMandy <maldonado.andy.m@gmail.com>
2023-01-18 09:54:36 -0500

    fixed scopes from sql injection

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,9 +22,9 @@ class User < ApplicationRecord
     end
   end
 
-  scope :filter_by_first_name, -> (first_name) { where(first_name: first_name)}
-  scope :filter_by_last_name, -> (last_name) {where(last_name: last_name)}
-  scope :filter_by_username, -> (username) {where(username: username)}
+  scope :filter_by_first_name, -> (first_name) { where("first_name LIKE?", first_name)}
+  scope :filter_by_last_name, -> (last_name) {where("laste_name LIKE?", last_name)}
+  scope :filter_by_username, -> (username) {where("username LIKE?", username)}
   
   def self.new_with_session(params, session)
     super.tap do |user|
