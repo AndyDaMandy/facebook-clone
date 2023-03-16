72abfae0d410cc3461551b76700fdeeb90cb188c
AndyDaMandy <maldonado.andy.m@gmail.com>
2023-03-16 11:08:24 -0400

    adjusted comment length

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,5 +2,5 @@ class Comment < ApplicationRecord
   belongs_to :post
   belongs_to :user
 
-  validates :comment, presence: true, length: { minimum: 1, maximum: 120 }
+  validates :comment, presence: true, length: { minimum: 1, maximum: 300 }
 end
