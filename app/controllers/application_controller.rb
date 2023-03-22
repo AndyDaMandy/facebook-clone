9ce6bf77f97490d72f6651f10917a0c17d04d03a
AndyDaMandy <maldonado.andy.m@gmail.com>
2023-03-22 09:24:50 -0400

    increased margins and padding all around

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,17 @@ class ApplicationController < ActionController::Base
 
     before_action :configure_permitted_parameters, if: :devise_controller?
 
+=begin  
+    around_action :set_time_zone
+
+    def set_time_zone
+      if user_signed_in?
+        Time.use_zone(current_user.time_zone) { yield }
+      else
+        yield
+      end
+    end
+=end
 
   protected
 
